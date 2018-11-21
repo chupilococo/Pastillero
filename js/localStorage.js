a1570342669750f5400f10e93d432152165b815c
chupilococo <danilo.carena@davinci.edu.ar>
2018-11-21 14:42:48 -0300

    Se arregla la estructura de carpetas

--- js/localStorage.js
+++ js/localStorage.js
@@ -7,10 +7,20 @@ $(document).on( "pageinit", "#misRemedios", function() {
 
 
 document.addEventListener("DOMContentLoaded",function () {
-   if (localStorage){
-   }else{
+    document.getElementById('formAdd').addEventListener('submit',function (e) {
+        e.preventDefault();
+        let desc    =$('#formDesc')[0];
+        let nombre  =$('#formNombre')[0];
+        dc.addRemedio({nombre: nombre.value,desc:desc.value});
+            desc.value='';
+            nombre.value='';
+        $.mobile.changePage('#misRemedios')
+    });
+
+    if (localStorage){
+    }else{
        console.log("local storage no disponible");
-   }
+    }
 });
 
 let dc={};
@@ -38,7 +48,7 @@ dc.addRemedio=function (remedio) {
         remedios.push(remedio);
         localStorage.setItem('remedios', JSON.stringify(remedios));
     }
-    dc.poblateMisRemedios(JSON.parse(dc.loadRemedios()));
+    // dc.poblateMisRemedios(JSON.parse(dc.loadRemedios()));
 };
 
 
@@ -46,35 +56,60 @@ dc.addRemedio=function (remedio) {
 /**
  *
  * @param remedio
+ * @param id
  * @returns {HTMLElement}
  */
-dc.createItemList=function(remedio){
+dc.createItemList=function(remedio, id){
 
     let itemList= document.createElement("li");
         itemList.dataset['role']='collapsible';
         itemList.dataset['inset']='false';
+        itemList.dataset['id']=id;
     let h2=document.createElement('h2');
         // noinspection JSUnresolvedVariable
-    h2.innerText=remedio.nombre;
+        h2.innerText=remedio.nombre;
     let div=document.createElement('div');
     let p=document.createElement('p');
         p.innerText=remedio.desc;
         div.appendChild(p);
+    let btn=document.createElement("button");
+        btn.innerText='borrar';
+        btn.addEventListener('click',function () {
+           dc.removeRemedio(
+               this.parentElement.parentElement.dataset['id']
+           );
+        });
         itemList.appendChild(h2);
         itemList.appendChild(div);
+        itemList.appendChild(btn);
     return itemList;
 };
+
+
+dc.removeRemedio=function(id){
+    let remedios=JSON.parse(dc.loadRemedios());
+        remedios.splice(id,1);
+        console.log(remedios);
+        localStorage.setItem('remedios', JSON.stringify(remedios));
+        dc.poblateMisRemedios(JSON.parse(dc.loadRemedios()));
+};
+
+
+
+
 
 /**
  *
  * @param remedios
  */
 dc.poblateMisRemedios=function (remedios) {
-    document.getElementById('misRemedios_lista').innerHTML='';
+    let misRemediosLista=$('#misRemedios_lista');
+        misRemediosLista[0].innerHTML='';
     for (let x in remedios) {
         // noinspection JSUnfilteredForInLoop
-        let child=dc.createItemList(remedios[x]);
-        $('#misRemedios_lista').append($(child).collapsible());
-    };
-    $('#misRemedios_lista').listview('refresh');
+        let child=dc.createItemList(remedios[x], x);
+        misRemediosLista.append($(child).collapsible());
+    }
+    misRemediosLista.listview('refresh');
 };
+
